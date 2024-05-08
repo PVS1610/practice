aa187e359b76720df10da397b74165b4d1cead25
PVS1610 <praveenvs1610@gmail.com>
2024-05-08 13:02:43 +0530

    11-ts-variables

---
+++ fullstack/frontend/languages/ts/datatypes/custom/myown.js
@@ -0,0 +1,10 @@
+var Human = /** @class */ (function () {
+    function Human(myeyes) {
+        this.eyes = myeyes;
+        this.ears = 2;
+        this.nose = 1;
+    }
+    return Human;
+}());
+var praveen = new Human(6);
+console.log(praveen);
